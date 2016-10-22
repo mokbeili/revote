30421f6b0f2d4449d1d04b06b3d1d7083e33f9a4
mokbeili <mo.kbeili@gmail.com>
2016-10-21 19:44:40 -0600

    Added address error notification

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,8 +1,16 @@
 class Voter < ApplicationRecord
   has_many :votes
 
+  validates_presence_of :longitude, { message: 'We were not able to find your address' }
+
+  validates :street, presence: true
+  validates :street_name, presence: true
+  validates :city, presence: true
+  validates :province, presence: true
+
   geocoded_by :address
-  after_validation :geocode
+
+  before_validation :geocode
 
   def address
     "#{street} #{street_name}, #{city}, #{province}"
